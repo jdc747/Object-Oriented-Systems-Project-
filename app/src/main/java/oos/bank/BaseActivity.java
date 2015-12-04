f747f1c9b0f75cc1e5f5fde779d0ff0ac24b840c
Garrett Huff <garretthuff41@yahoo.com>
2015-12-04 12:10:21 -0600

    no message

--- app/src/main/java/oos/bank/BaseActivity.java
+++ app/src/main/java/oos/bank/BaseActivity.java
@@ -27,6 +27,7 @@ public class BaseActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        //adding comment
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
