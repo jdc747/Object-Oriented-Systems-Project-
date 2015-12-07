96a4efc224784b3eebbc1f3f25eb4e86747d9995
Matthew <mlundy756@yahoo.com>
2015-12-07 14:52:58 -0600

    Adding updated chat code to repo with working authentication.

---
+++ app/src/main/java/com/oos/CS4773/socketio/FrontEnd/Chat/ChatActivityMain.java
@@ -0,0 +1,17 @@
+package com.oos.CS4773.socketio.FrontEnd.Chat;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.oos.CS4773.socketio.FrontEnd.R;
+
+
+public class ChatActivityMain extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
